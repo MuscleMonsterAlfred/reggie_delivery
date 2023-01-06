6c135987acae209d750b41edd9db03a834eee847
alfred <chenchenzhonghao@163.com>
2023-01-06 21:09:38 +0000

    框架搭建以及前端文件上传

---
+++ src/main/java/org/catarina/ReggieApplication.java
@@ -0,0 +1,10 @@
+package org.catarina;/**
+* @program: reggie_delivery
+* @ClassName ReggieApplication
+* @description: 
+* @author: alfred-chenzhonghao
+* @create: 2023-01-06 18:36
+* @Version 1.0
+**/
+public class ReggieApplication {
+}
---
+++ src/main/java/org/catarina/config/WebMvcConfig.java
@@ -0,0 +1,10 @@
+package org.catarina.config;/**
+* @program: reggie_delivery
+* @ClassName WebMvcConfig
+* @description: 
+* @author: alfred-chenzhonghao
+* @create: 2023-01-06 18:56
+* @Version 1.0
+**/
+public class WebMvcConfig {
+}
