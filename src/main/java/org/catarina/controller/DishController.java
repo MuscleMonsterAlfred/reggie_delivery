fcee8a2952063441aad1575072c9d933d9e37afd
alfred <chenchenzhonghao@163.com>
2023-01-21 02:30:32 +0000

    菜品管理分页功能

--- src/main/java/org/catarina/controller/DishController.java
+++ src/main/java/org/catarina/controller/DishController.java
@@ -9,18 +9,24 @@ package org.catarina.controller;
  * @Version 1.0
  **/
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.catarina.common.Result;
 import org.catarina.dto.DishDto;
+import org.catarina.entity.Category;
+import org.catarina.entity.Dish;
+import org.catarina.service.CategoryService;
 import org.catarina.service.DishFlavorService;
 import org.catarina.service.DishService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 菜品管理
@@ -37,19 +43,71 @@ public class DishController {
     @Autowired
     private DishFlavorService dishFlavorService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     /**
      * 新增菜品
-     * @param request
      * @param dishDto
      * @return
      */
     @PostMapping
-    public Result<String> save(HttpServletRequest request, @RequestBody DishDto dishDto){
+    public Result<String> save(@RequestBody DishDto dishDto){
         log.info(dishDto.toString());
 
         dishService.saveWithFlavor(dishDto);
 
         return Result.success("新增菜品成功");
+    }
+
+
+    /**
+     * 菜品信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public Result<Page> page(int page, int pageSize,String name){
+
+        //构造分页构造器对象
+        Page<Dish> pageInfo = new Page<>(page,pageSize);
+        Page<DishDto> dishDtoPage = new Page<>();
+        //条件构造器
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<Dish>();
+        //添加过滤条件
+        queryWrapper.like(name != null,Dish::getName,name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+
+        dishService.page(pageInfo,queryWrapper);
+
+        //拷贝数据
+        BeanUtils.copyProperties(pageInfo,dishDtoPage,"records");
+
+        List<Dish> records = pageInfo.getRecords();
+        List<DishDto> list = records.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+
+            BeanUtils.copyProperties(item,dishDto);
+            //讲道理这边完全可以用一个sql并表去查，数据量要是大的话还要遍历去查表增加db负担？
+            Long categoryId = item.getCategoryId();//分类id
+            //根据id查询分类对象
+            Category category = categoryService.getById(categoryId);
+            if(category != null){
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
+
+            return dishDto;
+
+        }).collect(Collectors.toList());
+
+        dishDtoPage.setRecords(list);
+
+        return Result.success(dishDtoPage);
+
     }
 
 }
