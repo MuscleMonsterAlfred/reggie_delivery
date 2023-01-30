2e73de243ce728c3ae59814c212ef9916dab19c3
alfred <chenchenzhonghao@163.com>
2023-01-30 20:27:31 +0000

    查看购物车以及清空购物车功能

--- src/main/java/org/catarina/controller/ShoppingCartController.java
+++ src/main/java/org/catarina/controller/ShoppingCartController.java
@@ -2,15 +2,16 @@ package org.catarina.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
 import org.catarina.common.BaseContext;
 import org.catarina.common.Result;
 import org.catarina.entity.ShoppingCart;
 import org.catarina.service.ShoppingCartService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @program: reggie_delivery
@@ -65,9 +66,41 @@ public class ShoppingCartController {
         }else{
             //如果不存在，则添加到购物车，数量默认就是1
             shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
             shoppingCartService.save(shoppingCart);
             cart = shoppingCart;
         }
         return Result.success(cart);
+    }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    @GetMapping("/list")
+    public Result<List<ShoppingCart>> list(){
+        log.info("查看购物车...");
+
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId,BaseContext.getCurrentId());
+        queryWrapper.orderByDesc(ShoppingCart::getCreateTime);
+
+        List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
+
+        return Result.success(list);
+    }
+
+    /**
+     * 清空购物车
+     * @return
+     */
+    @DeleteMapping("/clean")
+    public Result<String> clean(){
+        //根据用户id查找相应的购物车信息
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId,BaseContext.getCurrentId());
+
+        shoppingCartService.remove(queryWrapper);
+        return Result.success("已清空购物车");
     }
 }
